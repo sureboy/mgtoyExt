5b7bbca5e0995545b0ad42bf35b476fae885146b
zaddone <zaddone@qq.com>
2026-07-14 11:11:37 +0800

    bak

--- controlModule/js/udp.js
+++ controlModule/js/udp.js
@@ -1,5 +1,6 @@
 import {getDBFromAddr,setClientDB,postMessage} from './cache.js';
 const apMap = new Map();
+let isRun = false;
 window.addEventListener('message',(ev)=>{
     //console.log(ev.data);
     const {msg,udp} = ev.data;
@@ -18,17 +19,7 @@ window.addEventListener('message',(ev)=>{
         sendMsg.push(255); 
     }   
     //console.log(sendMsg,msg.length)
-    switch (msg.length) {
-        /*
-        case 1:
-            if (db){
-                db.Update = Date.now();
-                db.Num = msg[0];
-            }else{    
-                sendMsg.push(255); 
-            }               
-            break;
-    */
+    switch (msg.length) { 
         case 12: 
             const _db ={
                 Update:Date.now(),
@@ -40,18 +31,11 @@ window.addEventListener('message',(ev)=>{
                     RemotePort:udp.port,
                     Control: msg[1],
                 }
-            };
-            //console.log("client",_db);
+            }; 
             if (db){
                 Object.assign(db,_db);
             }else{
-                setClientDB(k,_db);
-                //addrMap.set(k,_db) ;
-                //nameMap.set(_db.DB.Carname,_db);
-                //if (conf.newCar){
-                //newCar?.(_db);
-                //console.log(_db)
-         
+                setClientDB(k,_db); 
                 postMessage({
                     menu:{
                         name:_db.DB.Carname,
@@ -63,89 +47,19 @@ window.addEventListener('message',(ev)=>{
             }
             break; 
         default: 
-            handleApList(msg);
-            break;
-            if ((msg.length >=9) && ((msg.length-2) %7 ===0)){ 
-                apMap.forEach(v=>{
-                    v.action <<=1; 
-                });
-                const status = [0,0];
-                let num = 0;
-                for (let i=2 ;i<msg.length;i+=7,num ++){ 
-                    const k = msg.subarray(i,i+6).toString(); 
-                    if (apMap.has(k)){
-                        const val = apMap.get(k);
-                        if (val.rssi.length>=5){
-                            val.rssi.shift();
-                            const _v = val.variations.shift();
-                            if (_v.wave){
-                                val.varSum -=_v.wave;
-                            }
-                        }
-                        val.rssi.push(msg[i+6]); 
-                        val.action |= 1;
-                        const [rssi1,rssi2] = val.rssi.slice(-2);  
-                        if ((val.action & 3) ===3){ 
-                            
-                            if (!rssi1 || !rssi2){
-                                console.log(val);
-                            }
-                            const variation = {wave:rssi1-rssi2  ,run:msg[1]};
-                            if (variation.wave< 0){
-                                //status[0]+=rssi2;
-                                variation.wave = -variation.wave;
-                            }
-                            const avg = (val.rssi.reduce((t,n)=>{
-                                return t +n ;
-                            })/val.rssi.length);
-                            console.log(avg-rssi2);
-                            status[1]+=variation.wave/avg;
-                            //console.log(status[1]);
-                             
-                            //if (variation.wave<0){
-                            //    variation.wave = - variation.wave;
-                            //}
-                            /*
-                            if (val.avg){
-                                let wv = val.avg - variation.wave;
-                                //wv = (wv<0)?-wv:wv;
-                                wv *= wv;
-                                if ( wv >val.waveAvg){ 
-                                    status[0]++;
-                                    console.log(  wv ,val.waveAvg,variation.wave);
-                                }else{
-                                    status[1]++;
-                                }
-                            }*/
-                            //avg = Math.sqrt(avg);
-                            val.variations.push(variation); 
-                            /*
-                            val.waveSum += variation.wave;
-
-                            val.avg = val.waveSum / val.variations.length;
-                            let avgsum = 0;
-                            val.variations.forEach((v,i_)=>{
-                                let _v = val.avg - v.wave;
-                                avgsum += _v*_v;
-                            });
-                            val.waveAvg = avgsum / val.variations.length; 
-                            */
-                        }else{
-                            //console.log(k);
-                            val.variations.push({});
-                            //status[1]+=rssi2;
-                        }
-                        //console.log(val);
-                    }else{
-                        apMap.set(k,{rssi:[msg[i+6]],action:1,variations:[],waveSum:0,waveAvg:0,avg:0 });
-                    }
+            handleApList(msg,()=>{
+                if (isRun){
+                    return;
                 }
-                console.log(status,status[1]/num);
-            }
-            //console.log(li);
-            //msg.subarray(1,msg.length)
-            //console.log((msg.length-1)/7,msg.toString());
-            //return;
+                isRun=true;
+                sendMsg.push(4);
+                setTimeout(()=>{
+                    postMessage({msg:new Uint8Array([sendMsg[0],8]),udp});
+                    isRun = false;
+                },1*1000);
+            });
+            break;
+             
     }
     postMessage({msg:new Uint8Array(sendMsg),udp});
     /*
@@ -155,11 +69,11 @@ window.addEventListener('message',(ev)=>{
         }
     });*/
 });
-const HISTORY_LEN = 5;
+const HISTORY_LEN = 3;
 const waveHistory = [];
-let waveHistorySum = 0;
-//let waveHistoryLen = 0;
-const handleApList = (msg)=>{
+let waveHistorySum = 0; 
+let waveAction = 1;
+const handleApList = (msg,isStop)=>{
     if ((msg.length < 9) || ((msg.length-2) %7 !==0)){ 
         return;
     } 
@@ -210,7 +124,7 @@ const handleApList = (msg)=>{
     
     waveList.forEach((v)=>{
         //console.log(v.rssi/sumRssi, v);
-        val +=   ( v.rssi/sumRssi )* v.wave;
+        val +=   ( v.avg/sumRssi )* v.wave;
     });
     
      
@@ -221,31 +135,33 @@ const handleApList = (msg)=>{
 
     
 
-    const rssiK = val/sumRssi;
-    if (!rssiK){return;}
+    const rssiWave = val/sumRssi;
+    if (!rssiWave){return;}
 
-    if (waveHistory.length===0){
-        waveHistory.push(rssiK);
-        waveHistorySum += rssiK;
+    if (waveHistory.length < HISTORY_LEN){
+        waveHistory.push(rssiWave);
+        waveHistorySum += rssiWave;
         //waveHistoryLen ++;
         return;
     }
-    const avg = waveHistorySum/waveHistory.length;
-    const fx = waveHistory.reduce((t,v)=>{
-        const _v = avg - v;
-         
-        return t + ((_v<0)?-_v:_v);
-    })/waveHistory.length;
-    let fx_ = rssiK - avg;
-    const isMoving = fx_ > fx;
-    if (msg[1]===0){
-        waveHistory.push(rssiK);
-        waveHistorySum += rssiK;
-        if (waveHistory.length>HISTORY_LEN*4){
-            waveHistorySum -= waveHistory.shift(); 
-        }
+    waveAction <<=1;
+    const waveAvg = waveHistorySum/waveHistory.length; 
+    waveHistory.push(rssiWave);
+    waveHistorySum += rssiWave;
+    const _waveAvg = waveHistorySum/waveHistory.length;
+    if ((_waveAvg > waveAvg) && (( waveAction & 2)!==2 )){
+        waveAction |=1; 
+    } 
+    if (waveHistory.length>HISTORY_LEN){
+        waveHistorySum -= waveHistory.shift(); 
     }
-    console.log(num,msg[1],isMoving,fx_,fx,waveHistory,rssiK );
+    const Stop =(( waveAction & 3) ===0);
+
+    console.log(num,msg[1],Stop,  rssiWave, waveAvg );
+    if (Stop && msg[1]!==0){
+        waveAction=1;
+        isStop();
+    }
     //WaveLast = waveNow;
 };
 function toInt8(byte) {
